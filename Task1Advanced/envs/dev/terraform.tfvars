4bb614264b95e852d0d87f67d045bc981e5eb343
Alex Dmitriev <a.dmitriev@corp.local>
2026-06-17 13:18:57 +0300

    Task1

---
+++ Task1Advanced/envs/dev/terraform.tfvars
@@ -0,0 +1,7 @@
+name      = "dev-vm"
+zone      = "ru-central1-a"
+cores     = 2
+memory    = 2
+disk_size = 20
+subnet_id = "replace-with-dev-subnet-id"
+ssh_key   = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdevExample"
---
+++ Task1Advanced/envs/stage/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "cores" {
+  type = number
+}
+
+variable "memory" {
+  type = number
+}
+
+variable "disk_size" {
+  type = number
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "ssh_key" {
+  type = string
+}
